de3a24e230aaf00c83a8ca067b184853a4452adc
Marco Viana <marcoviana100@gmail.com>
2023-09-17 17:22:54 -0300

    feat: implementação parcial da camada de Service e Controller

---
+++ app/backend/src/controllers/tool.controller.js
@@ -0,0 +1,49 @@
+const { getAllTools, getById, createTool, deleteById } = require('../services/tool.service');
+
+const listAllTools = async (req, res) => {
+    try {
+        const tools = await getAllTools();
+        res.status(200).json(tools);
+    } catch (err) {
+        res.status(500).json({ err: 'Erro ao listar ferramentas.'});
+    };
+};
+
+const getToolById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const tool = await getById(id);
+        if (tool) {
+            res.status(200).json(tool);
+        } else {
+            res.status(404).json({ message: 'Ferramenta não encontrada.'});
+        }
+
+    } catch (err) {
+        res.status(500).json({ err: 'Erro ao buscar ferramenta por id.'});
+    }
+};
+
+const createNewTool = async (req, res) => {
+    const toolData = req.body;
+    try {
+        const newTool = await createTool(toolData);
+        res.status(201).json(newTool);
+    } catch (err) {
+        res.status(500).json({ err: 'Erro ao criar uma nova ferramenta.' });
+    }
+};
+
+const deleteToolById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedTool = await deleteById(id);
+        if (deletedTool === 1) {
+            res.status(204).end();
+        } else {
+            res.status(404).json({ message: 'Ferramenta não encontrada para exclusão.' });
+        }
+    } catch (err) {
+        res.status(500).json({ err: 'Erro ao excluir a ferramenta por id.'})
+    };
+};
---
+++ app/backend/src/services/tool.service.js
@@ -0,0 +1,31 @@
+const ToolModel = require('../database/models/ToolModel');
+
+// Listando todas as ferramentas
+const getAllTools = async () => {
+    const tools = await ToolModel.findAll();
+    return tools;
+};
+
+// Retornando apenas uma por id
+const getById = async (id) => {
+    const tool = await ToolModel.findByPk(id);
+    return tool;
+};
+
+// Criando uma nova ferramenta
+const createTool = async (data) => {
+    const newTool = await ToolModel.create(data)
+    return newTool;
+};
+
+// Deletando uma ferramenta por id
+const deleteById = async (id) => {
+    const deletedTool = await ToolModel.destroy({
+        where: {
+            id: id,
+        },
+    });
+    return deletedTool;
+};
+
+export { getAllTools, getById, createTool, deleteById};
